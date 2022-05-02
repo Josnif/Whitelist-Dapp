1663b274699e86a22d8732498223d89da60caf94
joenif <josephnifemi89@gmail.com>
2022-05-02 16:36:01 +0100

    deploy changes

--- client/pages/index.js
+++ client/pages/index.js
@@ -202,7 +202,7 @@ export default function Home() {
       </div>
 
       <footer className={styles.footer}>
-        Made with &#10084; by Crypto Devs
+        Made with &#10084; by Joseph
       </footer>
     </div>
   );
